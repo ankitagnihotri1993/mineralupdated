2bdb19416b32d3b1d8b102973312f7377dd19605
Ankit <asingh@magicsoftware.com>
2018-08-06 18:56:43 +0530

    modified vendor trigger

---
+++ test.js
@@ -0,0 +1,17 @@
+
+//         var filterDate = new Date();
+//         filterDate.setDate(filterDate.getDate() - 1);
+        
+// var body=
+// {
+// body:{
+//     "view":"VENDOR",
+//     "query":"modified=ge="+filterDate.toLocaleString("yyyy-MM-dd'T'HH:mm:ssZ")
+// }};
+
+// console.log("body"+ JSON.stringify(body));
+
+var moment = require('moment');
+var now = new Date();
+var date= moment().format("YYYY-MM-DDTHH:mm:ssZ");
+console.log(date);
