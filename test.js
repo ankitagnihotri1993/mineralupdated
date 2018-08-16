8d9117b45dcc49c1d42c50e43a5caa3af6f77dab
Ankit <asingh@magicsoftware.com>
2018-08-16 22:12:09 +0530

    Added snapshot

--- test.js
+++ test.js
@@ -16,12 +16,30 @@
 // var date= moment().format("YYYY-MM-DDTHH:mm:ssZ");
 // console.log(date);
 
-var test={};
-if(test.id)
-{
- console.log('exits');
+// var test={};
+// if(test.id)
+// {
+//  console.log('exits');
 
-}
-else{
-    console.log('not exits');
-}
+// }
+// else{
+//     console.log('not exits');
+// }
+
+var moment = require('moment');
+var now = new Date();
+//let date = '12.01.2016 5:10:59'; //12 January 2016
+
+
+
+let parsedDate = moment((now.getDate()), 'DD-MM-YYYY H:mm:s')
+
+now.setDate(now.getDate() - 2);
+
+
+console.log(now.toISOString());
+
+// console.log(parsedDate.toISOString());
+
+//2018-08-16T13:10:57Z
+//2018-08-16T15:45:05.610Z
