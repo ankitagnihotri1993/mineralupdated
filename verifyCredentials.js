69a57689c4455c9f7b78680182a36e109787e8c3
ANKIT SINGH <me.ankitsingh1991@gmail.com>
2018-10-18 17:09:29 +0530

    update MIneral tree component

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,15 +1,19 @@
-"use strict";
-const request = require('request-promise');
-const messages = require('elasticio-node').messages;
-module.exports = verify;
-const accountService = require("./lib/commons/services/accountService.js");
-/***
- * @param credentials object to retrieve apiKey from
- *
- * @returns Promise sending HTTP request and resolving its response
- */
-async function verify(credentials) {
-    return await accountService.login(credentials);
-}
+'use strict';
 
+const MTService = require('./lib/MTService');
 
+module.exports = async function Verify(credentials) {
+    try {
+        const instance = new MTService(credentials, this);
+        const responce = await instance.verifyCredentials();
+        if (responce) {
+            console.log('Successfully verified credentials.');
+            return true;
+        }
+        console.log(`Error in validating credentials: ${JSON.stringify(responce)}`);
+        return false;
+    } catch (e) {
+        console.log(`Exception: ${e.toString()} \n ${e.stack}`);
+        return false;
+    }
+};
